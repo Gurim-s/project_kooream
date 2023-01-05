b3a548fe06d0d9f06f70b573528a89739b2d01ea
hayoung0406 <121089120+hayoung0406@users.noreply.github.com>
2023-01-05 09:37:50 +0900

    상품 리스트 불러오기

--- src/main/java/com/kooream/mapper/BrandProductUploadMapper.java
+++ src/main/java/com/kooream/mapper/BrandProductUploadMapper.java
@@ -4,8 +4,3 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.kooream.domain.AttachFileVO;
 
-@Mapper
-public interface BrandProductUploadMapper {
-	public int uploadFile(AttachFileVO vo);
-	public int removeFile(String fileName);
-}
