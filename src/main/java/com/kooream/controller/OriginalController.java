70e152fcd8d07696d94ef8f88d5fd96c8262e9d5
napxoxo <121083565+napxoxo@users.noreply.github.com>
2023-01-19 18:24:31 +0900

    1/19 정품판별

--- src/main/java/com/kooream/controller/OriginalController.java
+++ src/main/java/com/kooream/controller/OriginalController.java
@@ -1,6 +1,5 @@
 package com.kooream.controller;
 
-
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
@@ -25,47 +24,45 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/community/*")
 @AllArgsConstructor
 public class OriginalController {
-	
+
 	private OriginalService service;
-	
+
 	// 정품판별 게시판 리스트 화면 이동
 	@GetMapping("/oriList")
 	public String oriList(Model model) {
 		log.info("ori list........");
-		
+
 		model.addAttribute("list", service.oriList());
-		
+
 		return "/community/oriList";
 	}
-	
-	
+
 	// 정품판별 게시글 작성 화면 이동
 	@GetMapping("/oriRegister")
 	public String oriRegister() {
-		
+
 		return "/community/oriRegister";
 	}
-	
+
 	// 정품 판별 게시글 인서트
 	@PostMapping("/oriRegister")
 	public String oriRegister(OriginalVO vo) {
 		log.info("ori register....." + vo);
 		service.oriRegister(vo);
-		
+
 		List<OriginalAttachVO> list = vo.getAttachList();
-		
-		if(list.size()>0) {
-			for(OriginalAttachVO vo2 : list) {
+
+		if (list.size() > 0) {
+			for (OriginalAttachVO vo2 : list) {
 				log.info("------------------------filename : " + vo2.getFileName());
 				log.info("------------------------path : " + vo2.getUploadPath());
 				log.info("------------------------uuid : " + vo2.getUuid());
 			}
-			
+
 		}
-		
+
 		return "/community/oriList";
 	}
-	
 
 	@GetMapping(value = "/getAttachList", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseBody
@@ -73,18 +70,18 @@ public class OriginalController {
 		log.info("getAttachList....." + orino);
 		return new ResponseEntity<List<OriginalAttachVO>>(service.getAttList(orino), HttpStatus.OK);
 	}
-	
+
 	// 정품판별 게시글 조회
 	@GetMapping("/oriGet")
 	public String oriGet(int orino, Model model) {
 		log.info("original Get................" + orino);
+		OriginalVO vo = service.oriGet(orino);
 		
-		model.addAttribute("vo", service.oriGet(orino));
+		System.out.println("-------" + vo.getOrino());
+		System.out.println("-----" + vo.getAttachList().size());
+
+		model.addAttribute("vo",vo);
+		model.addAttribute("list", vo.getAttachList());
 		return "/community/oriGet";
 	}
-	
-	
-	
-	
-	
 }
--- src/main/webapp/WEB-INF/views/community/oriGet.jsp
+++ src/main/webapp/WEB-INF/views/community/oriGet.jsp
@@ -32,21 +32,64 @@
 	</ul>
 	<div>
 		<span id="head">정품판별</span>
-		<button>목록</button><button>수정</button>
+		<button data-oper="oriList">목록</button><button>수정</button>
 	</div>
 	<br/>
 	<hr/>
 	<br/>
 	<form action="">
-	<div>
-		<span>${vo.oriname }</span>
-		<span id="oriDate">${vo.oridate }</span>
-	</div>
-	<div>
-		
-	</div>
+		<div>
+			<span>${vo.oriname }</span>
+			<span id="oriDate">${vo.oridate }</span>
+		</div>
+		<div>
+			<c:if test="${vo.attachList.size() ne 0 }">
+				<c:forEach var="vo" items="${list }">
+					<c:url var="imgSrc" value="/display">
+						<c:param name="fileName" value="${vo.attachList.uploadPath }/${vo.attachList.uuid }_${vo.attachList.fileName }"></c:param>
+					</c:url>
+					<img alt="이미지" src="${imgSrc }">
+				</c:forEach>
+			</c:if>
+		</div>
+		<div>
+			${vo.oricon }
+		</div>
+		<div>
+			<span><a>진품 같아요 이미지 예정</a></span><span><a>가품 같아요 이미지 예정</a></span>
+		</div>
 	</form>
 	
+	<!-- 정품판별 댓글 내용 출력 폼 -->
+	<div>구리머들의 판정</div>
+	
+	
+	
+	
 </body>
+
+<script type="text/javascript">
+	$(function() {
+		//var form = $("#form");
+		
+		// 버튼 클릭 시 해당 작업 수행
+		$("button").click(function(e) {
+			e.preventDefault();
+			
+			var oper = $(this).data("oper");
+			if(oper == 'oriList'){
+				location.href='/community/oriList'
+			}
+			
+			
+			
+		})
+		
+		
+		
+		
+		
+	})
+</script>
 </html>
 <jsp:include page="../include/footer.jsp"/>
