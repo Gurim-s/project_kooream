d44079bf4bc264a3e18bae7c5c5ba4f536b91140
9ttttt <113842349+9ttttt@users.noreply.github.com>
2023-01-20 17:28:49 +0900

    1/20

--- src/main/java/com/kooream/domain/ProductVO.java
+++ src/main/java/com/kooream/domain/ProductVO.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class ProductVO {
 	private int p_no, b_no, p_release_price, p_stock, r_price;
-	private String p_name_ko, p_name_en, p_size, p_brand, p_model_no, p_category, p_class, r_check,strUuid;
+	private String p_name_ko, p_name_en, p_size, p_brand, p_model_no, p_category, p_class, r_check, strUuid;
 	private Date p_release_date;
 	private String img_url;
 	private String strt_r_date, rtrn_r_date;
