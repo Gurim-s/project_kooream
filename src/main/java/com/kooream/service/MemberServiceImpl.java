e9717f2374f66743d226ccfdb35f60d7701ed424
hayoung0406 <121089120+hayoung0406@users.noreply.github.com>
2023-01-30 09:50:38 +0900

    1/29

--- src/main/java/com/kooream/service/MemberServiceImpl.java
+++ src/main/java/com/kooream/service/MemberServiceImpl.java
@@ -1,8 +1,10 @@
 package com.kooream.service;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.kooream.domain.MemberVO;
@@ -10,13 +12,19 @@ import com.kooream.mapper.MemberMapper;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
+import oracle.jdbc.proxy.annotation.SetDelegate;
 
 @Service
 @Log4j
 public class MemberServiceImpl implements MemberService{
 	@Setter (onMethod_= @Autowired)
 	private MemberMapper mapper;
-
+	
+	// 패스워드 인코딩을 위해서 선언
+	@Setter (onMethod_= @Autowired)
+	private PasswordEncoder pwencoder;
+	
+	
 	@Override
 	public int check(Map<String,Object> map) {
 		return mapper.check(map);
@@ -24,9 +32,18 @@ public class MemberServiceImpl implements MemberService{
 
 	@Override
 	public int goJoin(MemberVO vo) {
-		return mapper.goJoin(vo);
+		// 패스워드 인코딩
+		vo.setM_pw(pwencoder.encode(vo.getM_pw()));
+		mapper.goJoin(vo);
+		
+		// 회원가입시 권한테이블에 m_no, auth 넣어주기
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("m_no", vo.getM_no());
+		map.put("auth", "ROLE_USER");
+		mapper.addAuth(map);
+		return 1;
 	}
-
+	/*
 	@Override
 	public int goLogin(Map<String,Object> map) {
 		return mapper.goLogin(map);
@@ -36,4 +53,5 @@ public class MemberServiceImpl implements MemberService{
 	public MemberVO getMemberInfo(Map<String, Object> map) {
 		return mapper.getMemberInfo(map);
 	}
+	*/
 }
--- src/main/webapp/WEB-INF/views/member/loginPage.jsp
+++ src/main/webapp/WEB-INF/views/member/loginPage.jsp
@@ -10,7 +10,7 @@
 					<span>아이디</span>
 				</td>
 				<td colspan="2">
-					<input type="text" name="m_id">
+					<input type="text" name="m_id"> <!-- 시큐리티에 m_id를 보내기위한 고정name -->
 				</td>
 			</tr>
 			<tr>
@@ -18,7 +18,9 @@
 					<span>비밀번호</span>
 				</td>
 				<td colspan="2">
-					<input type="password" name="m_pw">
+					<input type="password" name="m_pw"> <!-- 시큐리티에 m_pw를 보내기위한 고정name -->
+					<!-- 시큐리티 토큰 -->
+					<input type="hidden" name="${_csrf.parameterName }" value="${_csrf.token }"/>
 				</td>
 			</tr>
 			<tr>
@@ -45,6 +47,10 @@
 <script type="text/javascript" src='/resources/js/rental/slick.min.js'></script>
 <script  type="text/javascript">
 	$(function(){
+		// 로그인 실패시 문구 출력
+		if("${LoginFailMessage}"){
+			alert("${LoginFailMessage}")
+		}
 		// 회원가입 버튼 클릭이벤트
 		$("#joinBtn").on("click", function(e){
 			e.preventDefault();
@@ -54,9 +60,18 @@
 		// 로그인 버튼 클릭이벤트
 		$("#loginBtn").on("click", function(e){
 			e.preventDefault();
-			console.log("---")
-			$("#myForm").attr("action","/member/login");
-			$("#myForm").submit();
+			var idxx=0;
+			$('input').each(function (index, item) {
+			   if(!$(this).val()){
+				   alert($(this).parent().prev().children("span").text() + "을(를) 입력하세요.");
+				   idxx=1;
+				   return false;
+			   } 
+			});
+			if(idxx==0){
+				$("#myForm").attr("action","/login");
+				$("#myForm").submit();
+			}
 		});
 	});
 </script>
