708f93ecbabbc744f76b714676bbe5df502a5187
ty-oh <111411066+ty-oh@users.noreply.github.com>
2023-02-03 16:02:33 +0900

    gdgd

--- src/main/java/com/kooream/controller/SearchProductController.java
+++ src/main/java/com/kooream/controller/SearchProductController.java
@@ -6,12 +6,17 @@ import lombok.extern.log4j.Log4j;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.kooream.domain.ProductVO;
+import com.kooream.domain.SearchKeyword;
 import com.kooream.service.SearchProductService;
 
 @Log4j
@@ -20,4 +25,10 @@ import com.kooream.service.SearchProductService;
 public class SearchProductController {
 	SearchProductService service;
 	
+	@PostMapping(value = "/search/product", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity<List<ProductVO>> getList(@RequestBody SearchKeyword keyword) {
+		List<ProductVO> list = service.searchProductTag(keyword);
+		
+		return new ResponseEntity<List<ProductVO>>(list, HttpStatus.OK);
+	}
 }
--- src/main/webapp/resources/js/service/product-search-service.js
+++ src/main/webapp/resources/js/service/product-search-service.js
@@ -2,3 +2,27 @@
  * 
  */
 
+const productSearchService = (function() {
+	async function searchProduct(keyword) {
+		const result = await fetch('/search/product', {
+			method: 'POST',
+			headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+            },
+            body: JSON.stringify(keyword),
+		});
+		
+		return result.json();
+	}
+	
+	function getProduct(p_no) {
+		return {};
+	}
+	
+	return {
+		searchProduct: searchProduct,
+		getProduct: getProduct,
+	}
+})();
+
+export {productSearchService};
